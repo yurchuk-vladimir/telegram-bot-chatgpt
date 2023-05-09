612146efbc35f41a0f5013bde51279c67ab5c15b
Vladimir <vovadrak@gmail.com>
2023-05-10 01:35:02 +0300

    text and voice chatgpt

---
+++ Makefile
@@ -0,0 +1,8 @@
+build:
+	docker build -t tgbot .
+
+run:
+	docker run -d -p 3000:3000 --name tgbot --rm tgbot
+
+stop:
+	docker stop tgbot
---
+++ src/main.js
@@ -0,0 +1,81 @@
+import { Telegraf, session } from 'telegraf'
+import { message } from 'telegraf/filters'
+import { code } from 'telegraf/format'
+import config from 'config'
+import { ogg } from './ogg.js'
+import { openai } from './openai.js'
+
+const INITIAL_SESSION = {
+	messages: [],
+}
+
+const bot = new Telegraf(config.get('TELEGRAM_TOKEN'))
+
+bot.use(session())
+
+bot.command('new', async ctx => {
+	ctx.session = INITIAL_SESSION
+	await ctx.reply('Waiting your voice or text message')
+})
+
+bot.command('start', async ctx => {
+	ctx.session = INITIAL_SESSION
+	await ctx.reply('Waiting your voice or text message')
+})
+
+bot.on(message('voice'), async ctx => {
+	ctx.session ??= INITIAL_SESSION
+
+	try {
+		await ctx.reply(code('Waiting for server response...'))
+		const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id)
+		const userId = String(ctx.message.from.id)
+		const oggPath = await ogg.create(link.href, userId)
+		const mp3Path = await ogg.toMp3(oggPath, userId)
+
+		const text = await openai.transcription(mp3Path)
+		await ctx.reply(code(`Your request: ${text}`))
+
+		ctx.session.messages.push({ role: openai.roles.USER, content: text })
+
+		const response = await openai.chat(ctx.session.messages)
+
+		ctx.session.messages.push({
+			role: openai.roles.ASSISTANT,
+			content: response.content,
+		})
+
+		await ctx.reply(response.content)
+	} catch (e) {
+		console.log(`Error while voice message`, e.message)
+	}
+})
+
+bot.on(message('text'), async ctx => {
+	ctx.session ??= INITIAL_SESSION
+
+	try {
+		await ctx.reply(code('Waiting for server response...'))
+
+		ctx.session.messages.push({
+			role: openai.roles.USER,
+			content: ctx.message.text,
+		})
+
+		const response = await openai.chat(ctx.session.messages)
+
+		ctx.session.messages.push({
+			role: openai.roles.ASSISTANT,
+			content: response.content,
+		})
+
+		await ctx.reply(response.content)
+	} catch (e) {
+		console.log(`Error while text message`, e.message)
+	}
+})
+
+bot.launch()
+
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
